c77a9c17338d730d56a5ab6249f712617e1aca00
DanyalFaheem <DanyalFaheem@users.noreply.github.com>
2023-03-28 17:07:03 +0000

    [create-pull-request] automated change

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import praw
 import joblib
 import json
 import time
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 MNB = joblib.load("model")
 cv = joblib.load("vectorizer")
+
 
 def predict_sentiment(comment):
     sentiment_lookup = {-1: "negative", 0: "neutral", 1: "positive"}
@@ -15,36 +16,50 @@ def predict_sentiment(comment):
     predicted = MNB.predict(count_text)
     return sentiment_lookup[predicted[0]]
 
-@app.route('/')
+
+@app.route("/")
 def index():
     reddit = praw.Reddit(
         client_id="vkehb98SP7DXk-zpouJJPg",
         client_secret="Mh9_kBMneiQyl-aqYIoP6rNTMxaf3A",
-        user_agent="praw"
+        user_agent="praw",
     )
     reddit.read_only = True
 
     data = []
     count = 0
-    for comment in reddit.subreddit('all').stream.comments(skip_existing=True):
+    for comment in reddit.subreddit("all").stream.comments(skip_existing=True):
         prediction = predict_sentiment(comment)
-        data.append({"title": comment.submission.title, "comment": comment.body, "prediction": prediction})
+        data.append(
+            {
+                "title": comment.submission.title,
+                "comment": comment.body,
+                "prediction": prediction,
+            }
+        )
         count += 1
         if count >= 10:
             break
 
     while True:
         time.sleep(10)
-        for comment in reddit.subreddit('all').stream.comments(skip_existing=True):
+        for comment in reddit.subreddit("all").stream.comments(skip_existing=True):
             prediction = predict_sentiment(comment)
-            data.append({"title": comment.submission.title, "comment": comment.body, "prediction": prediction})
-            data = data[-10:] # keep only the last 10 comments
+            data.append(
+                {
+                    "title": comment.submission.title,
+                    "comment": comment.body,
+                    "prediction": prediction,
+                }
+            )
+            data = data[-10:]  # keep only the last 10 comments
             break
         else:
             continue
         break
 
-    return render_template('index.html', data=data)
+    return render_template("index.html", data=data)
+
 
 def predict_comment(comment):
     sentiment_lookup = {-1: "negative", 0: "neutral", 1: "positive"}
@@ -53,12 +68,13 @@ def predict_comment(comment):
     return sentiment_lookup[predicted[0]]
 
 
-@app.route('/search', methods=['POST'])
+@app.route("/search", methods=["POST"])
 def search():
-    comment = request.form['query']
+    comment = request.form["query"]
     prediction = predict_comment(comment)
-    data = [{"title":"Users comment","comment": comment, "prediction": prediction}]
-    return render_template('index.html', data=data)
+    data = [{"title": "Users comment", "comment": comment, "prediction": prediction}]
+    return render_template("index.html", data=data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
